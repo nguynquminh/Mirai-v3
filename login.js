5fc788a06075845d2ac0e3ee68ac10fe87e066fe
Nguyễn Minh <92246276+nguynquminh@users.noreply.github.com>
2025-04-04 02:19:04 +0700

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const fs = require("fs-extra");
 const readline = require("readline");
 const totp = require("totp-generator");
 const login = require("helyt");
-login({email: "thanh.doan.17.11.2000@gmail.com", password: "26082006Minh"}, (err, api) => {
+login({email: "123@gmail.com", password: "123"}, (err, api) => {
     if(err) return console.error(err);
     const json = JSON.stringify(api.getAppState());
     fs.writeFileSync(`./${config.APPSTATEPATH}`, json);
